2d20d7c1b6b6701a7ca2fbd14475924db601edce
Garry Ledwith <garryledwith@gmail.com>
2025-11-18 13:45:39 +0000

    added booking route to view all current bookings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,8 @@ import { EquipmentForm } from './equipment/equipment-form/equipment-form';
 import { UsersList } from './users/users-list/users-list';
 import { UserForm } from './users/users-form/users-form';
 import { UserDetail } from './users/user-detail/user-detail';
-
-//import { BookingsList } from './bookings/bookings-list/bookings-list';
+import { EquipmentDetailStudent } from './equipment/equipment-detail-student/equipment-detail-student';
+import { EquipmentBookinsList } from './equipment/equipment-bookings-list/equipment-bookings-list';
 
 // Application routes
 export const routes: Routes = [
@@ -20,13 +20,14 @@ export const routes: Routes = [
   { path: 'users/new', component: UserForm }, // Create new user
   { path: 'users/:id', component: UserDetail }, // User detail view by ID
   { path: 'users/:id/edit', component: UserForm }, // Edit user by ID
-  { path: 'users/role/:role', component: UsersList }, // List users by role (not implemented yet)
+  
 
   // Equipment routes
   { path: 'equipment', component: EquipmentList }, // List all equipment
-  { path: 'equipment/new', component: EquipmentForm }, // Create new equipment
+  { path: 'equipment/new', component: EquipmentForm }, // Create new equipment item
   { path: 'equipment/:id/edit', component: EquipmentForm }, // Edit equipment by ID
-  { path: 'equipment/:id/bookings', component: EquipmentList }, // List equipment bookings
+  { path: 'equipment/:id/bookings', component: EquipmentDetailStudent }, // Equipment detail view for students
+  { path: 'equipment/bookings', component: EquipmentBookinsList }, // List all equipment bookings
 
   // Fallback route
   { path: '**', redirectTo: '' }

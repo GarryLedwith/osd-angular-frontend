c98ed05d3bb662323166fbe36fa196d886ae7989
Garry Ledwith <garryledwith@gmail.com>
2025-12-10 07:20:27 +0000

    added authinterceptor for handling authenticating api requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(),
-
+    provideZonelessChangeDetection(), 
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    )
   ]
 };
 

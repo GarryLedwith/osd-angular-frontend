942d4c1b30d19954ae333ba9033b4a1e3bde3b9b
Garry Ledwith <garryledwith@gmail.com>
2025-12-15 20:16:42 +0000

    added adminGuard auth to some routes so only admins can access those routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { EquipmentBookingDetail } from './equipment/equipment-booking-detail/equ
 import { EquipmentBookinsList } from './equipment/equipment-bookings-list/equipment-bookings-list';
 import { LoginComponent } from './login/login';
 import { authGuard } from './auth/auth-guard';
+import { adminGuard } from './auth/auth-guard';
+
 
 // Application routes
 export const routes: Routes = [
@@ -23,15 +25,15 @@ export const routes: Routes = [
   
 
   // User routes
-  { path: 'users', component: UsersList, canActivate: [authGuard] }, // List all users
-  { path: 'users/new', component: UserForm, canActivate: [authGuard] }, // Create new user
+  { path: 'users', component: UsersList, canActivate: [authGuard, adminGuard] }, // List all users (admin only)
+  { path: 'users/new', component: UserForm, canActivate: [authGuard, adminGuard] }, // Create new user (admin only)
   { path: 'users/:id', component: UserDetail, canActivate: [authGuard] }, // User detail view by ID
-  { path: 'users/:id/edit', component: UserForm, canActivate: [authGuard] }, // Edit user by ID
+  { path: 'users/:id/edit', component: UserForm, canActivate: [authGuard, adminGuard] }, // Edit user by ID (admin only)
 
   // Equipment routes
-  { path: 'equipment', component: EquipmentList }, // List all equipment
-  { path: 'equipment/new', component: EquipmentForm, canActivate: [authGuard] }, // Create new equipment item
-  { path: 'equipment/:id/edit', component: EquipmentForm, canActivate: [authGuard] }, // Edit equipment by ID
+  { path: 'equipment', component: EquipmentList}, // List all equipment
+  { path: 'equipment/new', component: EquipmentForm, canActivate: [authGuard, adminGuard] }, // Create new equipment item (admin only)
+  { path: 'equipment/:id/edit', component: EquipmentForm, canActivate: [authGuard, adminGuard] }, // Edit equipment by ID (admin only)
   { path: 'equipment/:id/bookings', component: EquipmentBookingDetail, canActivate: [authGuard] }, // Equipment detail view for students
   { path: 'equipment/bookings', component: EquipmentBookinsList, canActivate: [authGuard] }, // List all equipment bookings
 

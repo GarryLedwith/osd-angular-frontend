690a894e5105edc4d8f83fef1c94112e6965fecf
Garry Ledwith <garryledwith@gmail.com>
2026-02-19 16:00:37 +0000

    added bookingLambdaUri

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUri: 'http://localhost:3000/api/v1'
+  apiUri: 'http://localhost:3000/api/v1',
+  bookingLambdaUri: 'https://WILL REPLACE_WITH_API_GATEWAY_INVOKE_URL'
 };
 

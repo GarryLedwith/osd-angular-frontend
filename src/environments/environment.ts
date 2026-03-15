9b0bd4724c93856d8cbc558ef72fe4d51622bcf5
Garry Ledwith <garryledwith@gmail.com>
2026-03-15 14:14:37 +0000

    Fixed docker setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUri: 'not ready yet',
+  apiUri: 'https://osd-project-api-production.up.railway.app/api/v1',
   bookingLambdaUri: 'https://bvukez92l4.execute-api.eu-west-1.amazonaws.com/prod',
 };
 
